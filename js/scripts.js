e34ebe1d1df76ab0535e54b074cc271442514c13
Angela Gathu <agathu87@gmail.com>
2017-08-21 13:56:12 +0300

    make changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
+jQuery(document).ready(function(){
+  $(".game").submit(function(event){
+    event.preventDefault()
+    var inputNum=parseInt($('input#inp').val());
+    console.log(inputNum);
+    bsns (inputNum)
+  });
+});
 
+
+
+
+
+var bsns = function(inputNum){
+  for (var i=1; i<=inputNum; i++){
+    if ((i%3 === 0)&&(i%5 === 0)){
+      console.log("pingpong");
+    }else if (i%5 === 0) {
+      console.log("pong");
+    }else if (i%3 === 0) {
+      console.log("ping");
+    }else {
+      console.log(i);
+    }
+  }
+};
