1b581b4ea296f9a4322fd92fdbd65600b7357202
Angela Gathu <agathu87@gmail.com>
2017-08-24 12:45:19 +0300

    will display the numbers, ping, pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,29 @@
-jQuery(document).ready(function(){
-  $(".game").submit(function(event){
-    event.preventDefault()
-    var inputNum=parseInt($('input#inp').val());
-    console.log(inputNum);
-    bsns (inputNum)
-  });
-});
+function list(inputNum) {
+  var itemList = []
+  for (var i = 1; i <= inputNum; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      itemList.push("pingpong");
+    } else if (i % 5 === 0) {
+      itemList.push("pong");
+    } else if (i % 3 === 0) {
+      itemList.push("ping");
+    } else {
+      itemList.push(i);
 
-
-
-
-
-var bsns = function(inputNum){
-  for (var i=1; i<=inputNum; i++){
-    if ((i%3 === 0)&&(i%5 === 0)){
-      console.log("pingpong");
-    }else if (i%5 === 0) {
-      console.log("pong");
-    }else if (i%3 === 0) {
-      console.log("ping");
-    }else {
-      console.log(i);
     }
   }
-};
+  return itemList
+}
+
+
+jQuery(document).ready(function() {
+  $(".game").submit(function(event) {
+    event.preventDefault()
+    var inputNum = parseInt($('input#inp').val());
+    var results = list(inputNum)
+    results.forEach(function(result) {
+      $("#outputList").append("<li>" + result + "</li>")
+    })
+
+  });
+});
